b154538d3911e3947cdfbc04c33cc7a6091be636
Miguel <jemiguelrivas@gmail.com>
2021-09-13 16:17:01 -0400

    adding jest

---
+++ tests/components/projects.spec.js
@@ -0,0 +1,38 @@
+import { mount } from "@vue/test-utils";
+import ProjectsWorkarea from "@/views/projects-workarea.vue";
+import ProjectsPanel from "@/views/projects-panel.vue";
+
+// describe('Gallery component unit tests: ', () => {
+//   test('is a Vue instance', () => {
+//     const wrapper = mount(Projects, {
+//       // propsData: {
+
+//       // }
+//     });
+//     expect(wrapper.isVueInstance()).toBeTruthy();
+//   });
+// });
+
+// import Gallery from "@/components/gallery.vue";
+
+// describe('Gallery component unit tests: ', () => {
+//   test('is a Vue instance', () => {
+//     const wrapper = mount(Gallery, {
+//       propsData: {
+//         db: [{
+//           "title": "olis",
+//           "category": "project",
+//           "client": "Presidente",
+//           "date": "2014/12/05",
+//           "type": "banana",
+//           "disabled": false,
+//           "links": [],
+//           "roles": ["president"],
+//           "tools": ["git"],
+//           "image": "https://miguel-rivas.github.io/zapp/img/preview.png"
+//         }]
+//       }
+//     });
+//     expect(wrapper.isVueInstance()).toBeTruthy();
+//   });
+// });
